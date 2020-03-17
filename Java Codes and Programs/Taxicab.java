a3f9fa5306c5fe33b3227b1ba76e8b1dd3c5650f
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-03-17 22:56:18 +0900

    Add files via upload

---
+++ Java Codes and Programs/Taxicab.java
@@ -0,0 +1,46 @@
+/* 
+Name: Wonho Lim 
+Class: APCS Section C 
+Date: 21 September 2019 
+Description: This program get the location of taxi first, get the moved distance by parameter
+and then update total
+*/ 
+public class Taxicab { //class Taxicab
+   private int x; //make field
+   private int y;//make field
+   private int total;//make field
+   
+   public Taxicab(){ //give basic values to fields
+      x = 0; //0
+      y = 0; //0
+      total = 0; //0
+   }
+   
+   public Taxicab(int xgo, int ygo){ //get parameters and update fields except total
+      x = xgo; //update
+      y = ygo; //update
+      total = 0; //total still 0
+   }
+   
+   public int getXCoord(){ ////getter of xcordination(return it)
+      return x; //return x
+   }
+   
+   public int getYCoord(){ //getter of ycordination(return it)
+      return y; //return y
+   }
+   
+   public int getDistanceTraveled(){ //getter of total
+      return total; //return total 
+   }
+   
+   public void moveX(int horizontal){ //get value from parameter and update fields
+      x = x + horizontal; //add distance
+      total = total + Math.abs(horizontal); //update total
+   }
+   
+   public void moveY(int vertical){ //get value from parameter and update fields
+      y = y + vertical; //add distance
+      total = total + Math.abs(vertical); //update total
+   }
+}
