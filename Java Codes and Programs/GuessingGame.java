8352222bf4bc1301ce8b71ab0d8c97e33566764d
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-03-18 21:48:48 +0900

    Update GuessingGame.java

--- Java Codes and Programs/GuessingGame.java
+++ Java Codes and Programs/GuessingGame.java
@@ -12,31 +12,31 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 		
-      haiku(); //print my haiku
+      		haiku(); //print my haiku
 		
 		int trial=0; //number of trials
 		int totalGames = 1; //total number of games
 		int totalGuess = 0; //total number of guesses
 		int bestScore = 0; //the best score that user made
-      final int MAX = 100; //maximum number to decalre the range of number
-      boolean check = true; //check whether the player wants to play again or not
-      char smallY = 'y'; //samll case y
-	   char capitalY = 'Y'; //capital case Y
+      		final int MAX = 100; //maximum number to decalre the range of number
+    		boolean check = true; //check whether the player wants to play again or not
+    		char smallY = 'y'; //samll case y
+	  	char capitalY = 'Y'; //capital case Y
       
-      while(check == true) {
+      		while(check == true) {
 			trial = 0; //reset trial to 0 for a new game 
-         int randomNumber = (int)(Math.random() * MAX) + 1; //picks a random number
+         		int randomNumber = (int)(Math.random() * MAX) + 1; //picks a random number
 			
-         System.out.println("I'm thinking of a number between 1 and " + MAX + "...");
-         Scanner in = new Scanner(System.in); //gets input from the user
+         		System.out.println("I'm thinking of a number between 1 and " + MAX + "...");
+         		Scanner in = new Scanner(System.in); //gets input from the user
 			System.out.print("Your guess? "); //asks the answer
 			int answer = in.nextInt();//gets user's answer
 			
-         trial = asking(randomNumber, answer, trial); //asking user new answer
+         		trial = asking(randomNumber, answer, trial); //asking user new answer
 			trial++; //add trial by 1 after guessing
 			
-         if(totalGames == 1) { //if it's a first game that the user had
-				bestScore = trial; //change best score to the first game's score
+         		if(totalGames == 1) { //if it's a first game that the user had
+					bestScore = trial; //change best score to the first game's score
 			}
 			if(trial < bestScore) { //if the score is even smaller than best score
 				bestScore = trial; //change best score to last game's score
@@ -65,14 +65,14 @@ public class GuessingGame {
 	}
 	
    //This method prints my haiku
-   public static void haiku(){
-     System.out.println("Hello, new player!");
-     System.out.println("This is a good guessing game.");
-     System.out.println("I hope you like it!");
-	}
+   		public static void haiku(){
+     		System.out.println("Hello, new player!");
+     		System.out.println("This is a good guessing game.");
+     		System.out.println("I hope you like it!");
+		}
    
-	//This is a method makes user to play game.
-	public static int asking(int randomNumber, int answer, int trial) {
+		//This is a method makes user to play game.
+		public static int asking(int randomNumber, int answer, int trial) {
 		Scanner in = new Scanner(System.in); //use scanner 
 		while(answer != randomNumber) { //while the answer and the number is different
 			  //infinitely asks until get a correct answer
@@ -81,7 +81,7 @@ public class GuessingGame {
 				System.out.print("Your guess? ");
 				answer = in.nextInt(); //ask the new answer
 			}  
-         if(answer < randomNumber) { //if answer is smaller than the number 
+         		if(answer < randomNumber) { //if answer is smaller than the number 
 				System.out.println("It's higher.");
 				System.out.print("Your guess? "); 
 				answer = in.nextInt(); //ask the new answer
