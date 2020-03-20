5209abce523b8a21edfc1f77bbe92964745dd339
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-03-20 22:06:14 +0900

    Update Taxicab.java

--- Java Codes and Programs/Taxicab.java
+++ Java Codes and Programs/Taxicab.java
@@ -2,8 +2,8 @@
 Name: Wonho Lim 
 Class: APCS Section C 
 Date: 21 September 2019 
-Description: This program get the location of taxi first, get the moved distance by parameter
-and then update total
+Description: This program get the location of taxi first, get the moved distance by parameter,
+and then update total distance
 *********************************************************************************************/ 
 public class Taxicab { //class Taxicab
    private int x; //make field
