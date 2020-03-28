c4ed87a7c080263f60703565dc60d7bac2d61651
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-03-28 11:52:09 +0900

    Update FindMedian.java

--- Java Codes and Programs/FindMedian.java
+++ Java Codes and Programs/FindMedian.java
@@ -6,21 +6,21 @@ import java.util.Arrays; //importing Array
 
 public class FindMedian {
   
-  public double FindMedian(int[] newArray, int arraySize){ //get two parameters
+  public double FindMedian(int[] newArray, int arraySize){ 
      
-     Arrays.sort(newArray); //sorting the array
+     Arrays.sort(newArray); 
      
      double median;
      
-     if(arraySize % 2 == 0){ //if there are even number of ints 
+     if(arraySize % 2 == 0){  
      
         median = ( newArray [ arraySize / 2 - 1 ] + newArray [ arraySize / 2 ] ) / 2.0; //calculation
         
         return median; //return  median
      }
      
-     median = newArray [ arraySize / 2 ]; //if there are odd number of ints
+     median = newArray [ arraySize / 2 ]; 
         
-     return median; //return median
+     return median; 
   }
 }
