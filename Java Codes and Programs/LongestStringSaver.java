ea6648ba30d3d10ff7c81f2574e8f68f4b67365c
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-03-28 11:51:39 +0900

    Update LongestStringSaver.java

--- Java Codes and Programs/LongestStringSaver.java
+++ Java Codes and Programs/LongestStringSaver.java
@@ -4,18 +4,17 @@ Description: This code prints out longest word that was typed by using sentinel 
 The program ifinifely repeats until the user put goodbye as a input. After user type
 goodbye, it will show length of greatest string that user have printed. 
 **************************************************************************************/
-
-//importing scanner for the program
 import java.util.Scanner;
-
 public class SentinelProgram {
    public static void main (String[] args) {
       Scanner scan = new Scanner(System.in);
       System.out.print("Type a word (or \"goodbye\" to quit): ");
       String line  = scan.nextLine();
+      
       int length = 0;
       int longestLength = 0;
       String longestString = "";
+      
       while (!line.equals("goodbye")){
          length = line.length();
          if (length > longestLength){ 
