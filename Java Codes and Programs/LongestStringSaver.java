03af07e902f3b7d8b1bf4c6d06f1ae173c731925
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-04-11 19:01:18 +0900

    Update LongestStringSaver.java

--- Java Codes and Programs/LongestStringSaver.java
+++ Java Codes and Programs/LongestStringSaver.java
@@ -1,6 +1,6 @@
 /************************************************************************************* 
 Name: Wonho Lim
-Description: This code prints out longest word that was typed by using sentinel value.
+Description: This code prints out longest word that was typed by the user by using sentinel value.
 The program ifinifely repeats until the user put goodbye as a input. After user type
 goodbye, it will show length of greatest string that user have printed. 
 **************************************************************************************/
