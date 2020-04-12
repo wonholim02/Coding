45c012a0ecb114b7dbf48b47b3bfee72dfd010af
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-04-12 21:45:09 +0900

    Update ConnectFour(GameBoard).java

--- Java Codes and Programs/ConnectFour(GameBoard).java
+++ Java Codes and Programs/ConnectFour(GameBoard).java
@@ -1,7 +1,7 @@
 /**********************************************************************************************
 Name: Wonho Lim 
 Description: This program contains methods that the client code can use to conduct the game and
-record and store everything that should be kept for
+record and store everything that should be kept for the
 **********************************************************************************************/ 
 public class GameBoard{
 	
