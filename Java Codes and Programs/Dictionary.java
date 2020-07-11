7b8967eb0af2a36514431fb182ae229e08383f04
Wonho Lim <62059099+wonholim02@users.noreply.github.com>
2020-07-11 23:19:50 +0900

    Update Dictionary.java

--- Java Codes and Programs/Dictionary.java
+++ Java Codes and Programs/Dictionary.java
@@ -1,5 +1,5 @@
 /*****************************************************************************************
-Coder Name: Wonho Lim 
+Coder Name: Wonho Lim TYPE:School Project
 Description: This program allows user to add, remove, change, get, display the word and its
 definition in dictionary as long as they want and then quit if they want to.
 ******************************************************************************************/
